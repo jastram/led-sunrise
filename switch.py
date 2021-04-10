bde44f9957f3a3feb32369b6696c75951152e8ca
Michael Jastram <michael@jastram.de>
2021-04-10 09:36:53 +0200

    Fixing bug on dimming sometimes not working

--- switch.py
+++ switch.py
@@ -19,11 +19,12 @@ pi = pigpio.pi()
 
 # Wait for button release 
 def dimming(status):
+  global last_tick
   lighteffects.toggle()
-  while switch_state == 0:
-    time.sleep(0.05)
+  while switch_state == 0: # Stop once released
+    time.sleep(0.05) # Dimming speed
     diff = abs(pi.get_current_tick() - last_tick)
-    if (diff > 1.7*1000000):
+    if (diff > 1.7*1000000): # Wait for a little
       if (status == 0): # Was off
         lighteffects.down()
       else: # Was on
@@ -44,6 +45,8 @@ def button(gpio, level, tick):
       if dimming_thread == None or not dimming_thread.is_alive():
         dimming_thread = threading.Thread(target=dimming, args=(lighteffects.is_on(),), kwargs={})
         dimming_thread.start()
+      else:
+        print("Skipping Dimming")
 
 pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
 pi.callback(SWITCH, pigpio.EITHER_EDGE, button)
